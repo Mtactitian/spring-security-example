c5a7c631645a8db6018ead5e080ab8ab8e3780ce
Alex B <brinza.alexey@gmail.com>
2018-06-03 14:20:39 +0300

    initial commit

---
+++ src/test/java/com/alexb/jdbcsecurity/JdbcSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexb.jdbcsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdbcSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
